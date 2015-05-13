96625baced2ed3da5aa5f181dda16482f23781ca
Jun Aramaki <junradio910@gmail.com>
2015-05-13 13:21:11 +0900

    各モデルの作成

---
+++ sample-app/db/migrate/20150513040726_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+  	　　
+  	　　t.references :user
+      t.references :category
+
+      t.timestamps
+    end
+  end
+end
